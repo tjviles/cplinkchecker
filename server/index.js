91e9fed08f42cf6cc6c55cbe83b862e361b3c561
tjviles <tjviles@gmail.com>
2022-10-22 10:22:30 -0500

    trying again

--- server/index.js
+++ server/index.js
@@ -131,7 +131,7 @@ app.post("/api", (req, res) => {
                 csvWriter.writeRecords(records)       // returns a promise
                     .then(() => {
                     console.log('...Done');
-                    res.send({badlinks: records[0]});
+                    res.send({badlinks: records[0].url});
                     });
             }
         }
